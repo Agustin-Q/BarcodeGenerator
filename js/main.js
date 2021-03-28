ad026c9fdaf33445b8eb5b1b9cff6701da052b99
Agustin-Q <agustin.queija@gmail.com>
2021-03-27 23:24:03 -0300

    Added PDF save

--- js/main.js
+++ js/main.js
@@ -5,14 +5,19 @@ class glyph{
         this.backgroundColor = "#ffffff";
     }
 
-    show(x,y,w,h){
+    show(x,y,w,h,showCtx){
+        
         let endPos = 0;
         let sum = 0;
         let currBar =1;
         for(let i = 0; i<this.pattern.length;i++){
-            this.pattern[i] == 0 ? ctx.fillStyle = this.backgroundColor : ctx.fillStyle = this.foregroundColor;
+            if (this.pattern[i] == 0){ 
+                showCtx.fillStyle ? showCtx.fillStyle = this.backgroundColor : showCtx.setFillColor(this.backgroundColor);
+            }else{ 
+                showCtx.fillStyle ? showCtx.fillStyle = this.foregroundColor : showCtx.setFillColor(this.foregroundColor);
+            }
             if (i == this.pattern.length -1||this.pattern[i+1]!= currBar){
-                ctx.fillRect(x+w*i-sum*w,y,w*(sum+1),h);
+                showCtx.fillRect(x+w*i-sum*w,y,w*(sum+1),h);
                 endPos = x+w*(i+1);
                 sum =0;
                 currBar = this.pattern[i+1];
@@ -26,6 +31,7 @@ class glyph{
 
 class barcode{
     constructor(str){
+        this.str = str;
         this.glyphs = [];
         this.chars = Array.from(str);
         this.glyphs.push(new glyph(104));
@@ -52,16 +58,16 @@ class barcode{
         return code128BTale[char.charCodeAt(0)];
     }
 
-    show(x,y,w,h){
+    show(x,y,w,h,showCtx){
         let pos = x;
         for(let i =0; i<this.glyphs.length; i++){
-            pos = this.glyphs[i].show(pos, y, w, h)
+            pos = this.glyphs[i].show(pos, y, w, h,showCtx)
         }
     }
 
-    showTotalWidth(x,y,totWidth,h){
+    showTotalWidth(x,y,totWidth,h,showCtx){
         let len = this.chars.length * 11 + 11*3+2;
-        this.show(x,y,totWidth/len,h);
+        this.show(x,y,totWidth/len,h,showCtx);
     }
 }
 
@@ -79,15 +85,15 @@ function drawBarcode(){
     let w = ctx.canvas.width;
     let h = ctx.canvas.height;
     let margins = 0.05;
-    barc.showTotalWidth(w*margins,w*margins,w-2*w*margins,h-2*w*margins);
-    showSVG("SVGContainer");
+    barc.showTotalWidth(w*margins,w*margins,w-2*w*margins,h-2*w*margins,ctx);
 }
 
 
 function init(){
-    ctx = new C2S(600,150);
     const input = document.getElementById("code");
+    ctx = document.getElementById("canvas").getContext("2d");
     input.addEventListener("input", onInput);
+    document.getElementById("saveButton").addEventListener("click",savePDF);
     const inputWidth = document.getElementById("width");
     const inputHeight = document.getElementById("height");
     inputWidth.value = ctx.canvas.width;
@@ -95,6 +101,7 @@ function init(){
     inputWidth.addEventListener("input", resize);
     inputHeight.addEventListener("input", resize);
     drawBarcode();
+    //savePDF();
     
 }
 
@@ -104,6 +111,7 @@ function resize(e){
     ctx.canvas.width = document.getElementById("width").value;
     ctx.canvas.height = document.getElementById("height").value;
     drawBarcode();
+    
 }
 
 function background(c){
@@ -112,11 +120,42 @@ function background(c){
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
 
-function showSVG(id){
-    let cont = document.getElementById(id);
-    cont.innerHTML = "";
-    cont.innerHTML = ctx.getSerializedSvg(true);
-}
 
 document.addEventListener('DOMContentLoaded', init);
 
+function fillRect(x,y,w,h){
+    this.rect(x,y,w,h,"F");
+}
+
+
+
+function savePDF(){
+    const { jsPDF } = window.jspdf;
+
+    // Default export is a4 paper, portrait, using millimeters for units
+    const doc = new jsPDF({
+        orientation: "landscape",
+        unit: "mm",
+        format: [150, 100]
+    });    
+    doc.fillRect = fillRect;
+
+    //doc.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    //doc.fillStyle = "#ffffff";
+    //doc.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    let w = 150;
+    let h = 100;
+    let margins = 0.05;
+    barc.showTotalWidth(w*margins,w*margins,w-2*w*margins,h-2*w*margins,doc);
+    //doc.fillStyle("#ffff00");
+    //doc.fillRect(30,30,30,30);
+    //doc.addImage(ctx.canvas, "SVG", 0,0,150,100);
+    let opt = {
+        align: "center",
+        baseline: "top",
+    }
+    doc.setFillColor(255);
+    doc.rect(w/2-40, h*0.94, 80,10,"F");
+    doc.text(barc.str, w/2, h*0.94, opt);
+    doc.save("test.pdf");
+}
